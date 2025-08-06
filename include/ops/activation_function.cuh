460d37eff4c3e102c4f1aae01ea2d2c3892674cd
LHOOL1109 <shanghai110930@gmail.com>
2025-08-06 09:39:31 +0900

    update: add wrapper function

--- include/ops/activation_function.cuh
+++ include/ops/activation_function.cuh
@@ -3,6 +3,12 @@ activation_function.cuh
 */
 #pragma once
 
-__global__ void step_function(const float* input, float* output, int size);
-__global__ void sigmoid_function(const float* input, float* output, int size);
-__global__ void relu_function(const float* input, float* output, int size);
+__global__ void step_kernel(const float* input, float* output, int size);
+
+__global__ void sigmoid_kernel(const float* input, float* output, int size);
+void sigmoid(const Tensor& input, Tensor& output);
+Tensor sigmoid(const Tensor& input);
+
+__global__ void relu_kernel(const float* input, float* output, int size);
+void relu(const Tensor& input, Tensor& output);
+Tensor relu(const Tensor& input);
--- src/ops/activation_function.cu
+++ src/ops/activation_function.cu
@@ -1,24 +1,75 @@
 #include <cuda_runtime.h>
-#include "../../include/ops/activation_function.cuh"
-#include "../../include/utils/cuda_utils.cuh"
+#include <core/tensor.cuh>
+#include "ops/activation_function.cuh"
+#include "utils/cuda_utils.cuh"
 
-__global__ void step_function(const float* input, float* output, int size)
+__global__ void step_kernel(const float* input, float* output, int size)
 {
     int idx = CUDA_1D_IDX;
     if (idx >= size) return;
     output[idx] = (input[idx] > 0) ? 1 : 0;
 }
 
-__global__ void sigmoid_function(const float* input, float* output, int size)
+__global__ void sigmoid_kernel(const float* input, float* output, int size)
 {
     int idx = CUDA_1D_IDX;
     if (idx >= size) return;
     output[idx] = 1 / (1 + expf(-input[idx]));
 }
 
-__global__ void relu_function(const float* input, float* output, int size)
+void sigmoid(const Tensor& input, Tensor& output)
+{
+    if (input.size() != output.size())
+        throw std::runtime_error("relu: size mismatch");
+
+    int size = input.size();
+    int block_size = 256;
+    int grid_size = (size + block_size - 1) / block_size;
+
+    sigmoid_kernel<<<grid_size, block_size>>>(
+        input.device_ptr(),
+        output.device_ptr(),
+        size
+    );
+    cudaDeviceSynchronize();
+}
+
+Tensor sigmoid(const Tensor& input)
+{
+    Tensor output(input.batchs(), input.channels(), input.height(), input.width());
+    sigmoid(input, output);
+    return output;
+}
+
+
+__global__ void relu_kernel(const float* input, float* output, int size)
 {
     int idx = CUDA_1D_IDX;
     if (idx >= size) return;
     output[idx] = (input[idx] > 0) ? input[idx] : 0;
+}
+
+
+void relu(const Tensor& input, Tensor& output)
+{
+    if (input.size() != output.size())
+        throw std::runtime_error("relu: size mismatch");
+
+    int size = input.size();
+    int block_size = 256;
+    int grid_size = (size + block_size - 1) / block_size;
+
+    relu_kernel<<<grid_size, block_size>>>(
+        input.device_ptr(),
+        output.device_ptr(),
+        size
+    );
+    cudaDeviceSynchronize();
+}
+
+Tensor relu(const Tensor& input)
+{
+    Tensor output(input.batchs(), input.channels(), input.height(), input.width());
+    relu(input, output);
+    return output;
 }
